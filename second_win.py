d701d57b478f0b7cb24cc80551be7cb4e8ff9ae7
Huang Na <huangnadya7@gmail.com>
2026-02-13 19:26:25 +0700

    Update second_win.py

--- second_win.py
+++ second_win.py
@@ -1,1 +1,22 @@
 # write a code for the second screen of app
+
+def timer_test(self):
+    global time
+    time = QTime(0, 0, 15)
+    self.timer = QTimer()
+    self.timer.timeout.connect(self.timer1Event)
+    self.timer.start(1000)
+def timer_sits(self):
+    global time
+    time = QTime(0, 0, 30)
+    self.timer = QTimer()
+    self.timer.timeout.connect(self.timer2Event)
+    #one squat in 1.5 seconds
+    self.timer.start(1500)
+def timer_final(self):
+    global time
+    time = QTime(0, 1, 0)
+    self.timer = QTimer()
+    self.timer.timeout.connect(self.timer3Event)
+    #one squat in 1.5 seconds
+    self.timer.start(1000)
